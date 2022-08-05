91482a1a176e7c71dd3403b082045e82c86d449e
iruojefaith <fumsy240@gmail.com>
2022-08-05 03:31:28 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+//get element by id
+$(document).ready(function(){
+           $('.button').click(function(){
+             if($(this).hasClass('expand')){
+               $('ul').slideUp(function(){
+                 $('.button').removeClass('expand');
+                 $('.fas').removeClass('expand')
+               });
+             }else{
+               $(this).addClass('expand');
+               setTimeout(function(){
+                 $('.fas').addClass('expand');
+                 $('ul').stop().slideDown();
+               },200);
+             }
+           });
+         });
